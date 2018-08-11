f93358fbc89f6da0d34e4754f1cd88bcbc0d8615
Radoslaw Hulboj <radekhulboj@gmil.com>
2018-08-11 15:21:23 +0200

    The gradle build is working

---
+++ src/main/java/cs/business/OperationalEvent.java
@@ -0,0 +1,8 @@
+package cs.business;
+
+/**
+ * Created by radoslaw on 11.08.18.
+ * sort, find short event, long
+ */
+public class OperationalEvent {
+}
---
+++ src/main/java/cs/database/PersistEvent.java
@@ -0,0 +1,30 @@
+package cs.database;
+
+import cs.domain.Event;
+
+/**
+ * store to hsqldb
+*/
+
+public class PersistEvent implements ICrudeEvent{
+
+    @Override
+    public void save(Event event) {
+
+    }
+
+    @Override
+    public void delete(Event event) {
+
+    }
+
+    @Override
+    public void update(Event event) {
+
+    }
+
+    @Override
+    public void insert(Event event) {
+
+    }
+}
