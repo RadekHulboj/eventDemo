be953e9c63bb2aaf6c12efdc8ea0cc31f5f1bfc6
Radoslaw Hulboj <radoslaw.hulboj@axxiome.com>
2018-08-13 15:04:47 +0200

    The clean code (TODO: get resource from fatJar )

--- src/main/java/cs/main/EventSystemApplication.java
+++ src/main/java/cs/main/EventSystemApplication.java
@@ -13,18 +13,34 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EventSystemApplication {
 
-  public static void main(String[] args) throws SQLException, IOException, URISyntaxException {
-    createEventDbTable();
+  private static Logger logger = LoggerFactory.getLogger(EventSystemApplication.class);
 
-    final IDaoEvent persistEvent = new PersistEvent();
-    addEventsToDb(getLogEventsFromJson(), persistEvent);
-    List<Event> dbEvents = persistEvent.read();
+  public static void main(String[] args) throws SQLException, IOException, URISyntaxException {
+    resetDatabaseEachTimeExecuted();
+    addEventsToDbFrom(getLogEventsFromJson());
+    printEventsFromDb();
   }
 
-  private static void addEventsToDb(Event[] events, IDaoEvent persistEvent) {
+  private static void resetDatabaseEachTimeExecuted() throws URISyntaxException, IOException, SQLException {
+    executeStatementFromSql("db_sql/event.sql");
+    executeStatementFromSql("db_sql/eventDeleteAll.sql");
+  }
+
+  private static void printEventsFromDb() throws SQLException {
+    final IDaoEvent persistEvent = new PersistEvent();
+    List<Event> dbEvents = persistEvent.read();
+    dbEvents.stream().forEach(event -> logger
+        .info(String.format("Id: %s timestamp: %s", event.getId(), event.getTimestamp().toString()))
+    );
+  }
+
+  private static void addEventsToDbFrom(Event[] events) {
+    final IDaoEvent persistEvent = new PersistEvent();
     Arrays.stream(events).forEach((event) -> {
       try {
         persistEvent.insert(event);
@@ -39,9 +55,9 @@ public class EventSystemApplication {
     return jsonParser.createFromJson("logs/log.json");
   }
 
-  private static void createEventDbTable() throws URISyntaxException, IOException, SQLException {
+  private static void executeStatementFromSql(String pathToSql) throws URISyntaxException, IOException, SQLException {
     FileService fileService = new FileService();
-    String createEventTableStr = fileService.getStringFrom("db_sql/event.sql");
+    String createEventTableStr = fileService.getStringFrom(pathToSql);
     try (Statement statement = JDBCHelper.getConnection().createStatement()) {
       statement.execute(createEventTableStr);
     }
--- src/main/java/cs/service/FileService.java
+++ src/main/java/cs/service/FileService.java
@@ -5,12 +5,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
@@ -18,14 +14,14 @@ import org.slf4j.LoggerFactory;
 
 public class FileService {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(FileService.class);
+    private static Logger logger = LoggerFactory.getLogger(FileService.class);
 
     public File getFileFrom(String path) throws URISyntaxException, IOException {
         return new File(getUri(path));
     }
 
     public String getStringFrom (String path) throws URISyntaxException, IOException {
-        LOGGER.info(String.format("Path: %s", path));
+        logger.info(String.format("Path: %s", path));
         String collectStr;
         try(Stream<String> lines = Files.lines(Paths.get(getUri(path)))){
             collectStr = lines.parallel().map(String::trim).collect(Collectors.joining());
