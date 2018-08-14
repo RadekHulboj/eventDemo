008bc319070405d6928369d366dc86bde2586820
Radoslaw Hulboj <radoslaw.hulboj@axxiome.com>
2018-08-14 16:19:59 +0200

    The fatJar BUG almost solved

--- src/main/java/cs/service/FileService.java
+++ src/main/java/cs/service/FileService.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
@@ -32,6 +35,25 @@ public class FileService {
 
   private URI getUri(String path) throws URISyntaxException, IOException {
     URL resource = getClass().getClassLoader().getResource(path);
-    return resource.toURI();
+
+    URI uri = resource.toURI();
+    if("jar".equals(uri.getScheme())){
+      logger.info("Uri schema" + uri.getScheme());
+      for (FileSystemProvider provider: FileSystemProvider.installedProviders()) {
+        if (provider.getScheme().equalsIgnoreCase("jar")) {
+          try {
+            logger.info("Current uri: " + uri.toString());
+            provider.getFileSystem(uri);
+          } catch (FileSystemNotFoundException e) {
+            // in this case we need to initialize it first:
+            logger.error(e.toString());
+            provider.newFileSystem(uri, Collections.emptyMap());
+            getUri(path);
+          }
+        }
+      }
+    }
+
+    return uri;
   }
 }
